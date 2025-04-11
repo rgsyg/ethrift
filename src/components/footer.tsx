4703eb7f36badf320fbc59340e6ac4ef5189ea75
rgsyg <rigorsyguatbsn@gmail.com>
2025-04-11 14:39:12 +0800

    modify style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="bg-secondary h-16 flex justify-center items-center">
+    <footer className="bg-secondary h-16 flex justify-center items-center mt-28">
       &copy; eThrift 2025
     </footer>
   );
