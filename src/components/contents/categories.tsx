24616eee166d15958659dd2e44ab82cbb55037fa
rgsyg <rigorsyguatbsn@gmail.com>
2025-04-11 14:38:57 +0800

    modify style

--- src/components/contents/categories.tsx
+++ src/components/contents/categories.tsx
@@ -28,13 +28,17 @@ export default function Categories() {
   ];
 
   return (
-    <section className="space-y-8 bg-secondary rounded-2xl p-6 pb-8">
+    <section className="space-y-8 bg-secondary rounded-2xl p-6 pb-10">
       <h1 className="text-4xl text-center font-bold">Categories</h1>
       <div className="flex gap-4 justify-around">
         {categories.map((category) => (
           <Card key={category.categoryName} className="w-72">
             <CardHeader>
-              <Image src={category.categoryImg} alt="Category alt" />
+              <Image
+                src={category.categoryImg}
+                alt="Category alt"
+                className="aspect-square object-cover"
+              />
             </CardHeader>
             <CardFooter>
               <p className="mx-auto font-bold text-2xl">
